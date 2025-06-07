b64f946e1b2c4d9c43220bc073c2af18bd8a6b36
Kumaraswamy888 <kumaraswamy1318@gmail.com>
2025-06-07 11:45:06 +0530

    Update TC_DWS_001_Test.java

--- selenium.Maven/src/test/java/books/TC_DWS_001_Test.java
+++ selenium.Maven/src/test/java/books/TC_DWS_001_Test.java
@@ -18,6 +18,5 @@ public class TC_DWS_001_Test extends BaseClass{
 		hp.getBooksLink().click();
 		Assert.assertEquals(driver.getTitle(), "Demo Web Shop. Books");
 		test.log(Status.PASS, "Books page is displayed");
-		test.log(Status.PASS, "Books page is displayed");
 	}
 }
